009c5ef947f74e3ddb74e93577b5527373916f85
jdragon14 <ede7272@naver.com>
2019-05-26 12:21:04 +0900

    first2

---
+++ Assets/Scripts/BackgroundTile.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundTile : MonoBehaviour
+{
+    public GameObject[] dots;
+
+    void Start()
+    {
+        Initialize();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Initialize()
+    {
+        int dotToUse = Random.RandomRange(0, dots.Length);
+        GameObject dot = Instantiate(dots[dotToUse], transform.position, Quaternion.identity);
+        dot.transform.parent = this.transform;
+        dot.name = this.gameObject.name;
+    }
+}
